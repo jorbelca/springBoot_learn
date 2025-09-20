ce483075c2a1e1492a4cbe0e67bf34fd6c64416e
j <76847923+jorbelca@users.noreply.github.com>
2025-09-20 13:52:38 +0200

    mongo done

---
+++ 6/planefinder/src/main/kotlin/com/nosql/planefinder/AircraftRepository.kts
@@ -0,0 +1,4 @@
+
+
+
+interface AircraftRepository : CrudRepository<Aircraft, String>
---
+++ 6/planefinder/src/test/kotlin/com/nosql/planefinder/PlanefinderApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.nosql.planefinder
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PlanefinderApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
