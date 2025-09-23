961e90828498d0cdba884ba53b5cc7a3a1a1f504
j <76847923+jorbelca@users.noreply.github.com>
2025-09-23 17:39:34 +0200

    capitulo 7 finalizado

--- 7/aircraft-positions/src/main/java/com/thehecklers/aircraftpositions/PositionController.java
+++ 7/aircraft-positions/src/main/java/com/thehecklers/aircraftpositions/PositionController.java
@@ -4,8 +4,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import com.thehecklers.aircraftpositions.AircraftRepository;
-
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 
--- 7/aircraft-positions/src/main/java/com/thehecklers/aircraftpositions/PositionRetriever.java
+++ 7/aircraft-positions/src/main/java/com/thehecklers/aircraftpositions/PositionRetriever.java
@@ -1,28 +1,43 @@
-
 package com.thehecklers.aircraftpositions;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.function.Consumer;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import com.thehecklers.aircraftpositions.Aircraft;
-import com.thehecklers.aircraftpositions.AircraftRepository;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
 
 import lombok.AllArgsConstructor;
 
 @AllArgsConstructor
 @Configuration
 public class PositionRetriever {
-    private final AircraftRepository repo;
+    private final AircraftRepository repository;
+    private final WebSocketHandler handler;
 
     @Bean
     Consumer<List<Aircraft>> retrieveAircraftPositions() {
         return acList -> {
-            repo.deleteAll();
-            repo.saveAll(acList);
-            repo.findAll().forEach(System.out::println);
+            repository.deleteAll();
+
+            repository.saveAll(acList);
+
+            sendPositions();
         };
+    }
+
+    private void sendPositions() {
+        if (repository.count() > 0) {
+            for (WebSocketSession sessionInList : handler.getSessionList()) {
+                try {
+                    sessionInList.sendMessage(
+                            new TextMessage(repository.findAll().toString()));
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 }
