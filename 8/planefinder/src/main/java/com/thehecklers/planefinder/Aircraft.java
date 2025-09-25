c9ba0207a98c71a7ac8d77cf56f88fabf6b1e3ce
j <76847923+jorbelca@users.noreply.github.com>
2025-09-25 18:18:27 +0200

    capitulo 8

---
+++ 8/planefinder/src/main/java/com/thehecklers/planefinder/Aircraft.java
@@ -0,0 +1,101 @@
+package com.thehecklers.planefinder;
+
+import java.time.Instant;
+
+import org.springframework.data.annotation.Id;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class Aircraft {
+    @Id
+    private Long id;
+    private String callsign, squawk, reg, flightno, route, type, category;
+
+    private int altitude, heading, speed;
+    @JsonProperty("vert_rate")
+    private int vertRate;
+    @JsonProperty("selected_altitude")
+    private int selectedAltitude;
+
+    private double lat, lon, barometer;
+    @JsonProperty("polar_distance")
+    private double polarDistance;
+    @JsonProperty("polar_bearing")
+    private double polarBearing;
+
+    @JsonProperty("is_adsb")
+    private boolean isADSB;
+    @JsonProperty("is_on_ground")
+    private boolean isOnGround;
+
+    @JsonProperty("last_seen_time")
+    private Instant lastSeenTime;
+    @JsonProperty("pos_update_time")
+    private Instant posUpdateTime;
+    @JsonProperty("bds40_seen_time")
+    private Instant bds40SeenTime;
+
+    // Constructor completo explícito (sustituye al generado por Lombok)
+    public Aircraft(Long id,
+            String callsign, String squawk, String reg, String flightno,
+            String route, String type, String category,
+            int altitude, int heading, int speed,
+            int vertRate, int selectedAltitude,
+            double lat, double lon, double barometer,
+            double polarDistance, double polarBearing,
+            boolean isADSB, boolean isOnGround,
+            Instant lastSeenTime, Instant posUpdateTime, Instant bds40SeenTime) {
+        this.id = id;
+        this.callsign = callsign;
+        this.squawk = squawk;
+        this.reg = reg;
+        this.flightno = flightno;
+        this.route = route;
+        this.type = type;
+        this.category = category;
+        this.altitude = altitude;
+        this.heading = heading;
+        this.speed = speed;
+        this.vertRate = vertRate;
+        this.selectedAltitude = selectedAltitude;
+        this.lat = lat;
+        this.lon = lon;
+        this.barometer = barometer;
+        this.polarDistance = polarDistance;
+        this.polarBearing = polarBearing;
+        this.isADSB = isADSB;
+        this.isOnGround = isOnGround;
+        this.lastSeenTime = lastSeenTime;
+        this.posUpdateTime = posUpdateTime;
+        this.bds40SeenTime = bds40SeenTime;
+    }
+
+    public Aircraft(String callsign, String reg, String flightno, String type,
+            int altitude, int heading, int speed,
+            double lat, double lon) {
+        this(null, callsign, "sqwk", reg, flightno, "route", type, "ct",
+                altitude, heading, speed, 0, 0,
+                lat, lon, 0D, 0D, 0D,
+                false, true,
+                Instant.now(), Instant.now(), Instant.now());
+    }
+
+    public void setLastSeenTime(long lastSeenTime) {
+        this.lastSeenTime = Instant.ofEpochSecond(lastSeenTime);
+    }
+
+    public void setPosUpdateTime(long posUpdateTime) {
+        this.posUpdateTime = Instant.ofEpochSecond(posUpdateTime);
+    }
+
+    public void setBds40SeenTime(long bds40SeenTime) {
+        this.bds40SeenTime = Instant.ofEpochSecond(bds40SeenTime);
+    }
+}
