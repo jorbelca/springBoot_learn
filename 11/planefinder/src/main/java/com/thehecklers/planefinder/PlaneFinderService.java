8d9f42a18b04f477468033c5e6e69a7643239cf7
j <76847923+jorbelca@users.noreply.github.com>
2025-09-29 18:10:24 +0200

    11 end

--- 11/planefinder/src/main/java/com/thehecklers/planefinder/PlaneFinderService.java
+++ 11/planefinder/src/main/java/com/thehecklers/planefinder/PlaneFinderService.java
@@ -6,25 +6,29 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.SneakyThrows;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
+import java.net.MalformedURLException;
 
 import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
-
+import java.net.MalformedURLException;
 @Service
 public class PlaneFinderService {
     private final PlaneRepository repo;
     private URL acURL;
     private final ObjectMapper om;
 
-    @SneakyThrows
     public PlaneFinderService(PlaneRepository repo) {
-        this.repo = repo;
-
-        acURL = new URL("http://192.168.1.193/ajax/aircraft");
-        om = new ObjectMapper();
-    }
+           this.repo = repo;
+   
+           try {
+               acURL = new URL("http://192.168.1.193/ajax/aircraft");
+           } catch (MalformedURLException e) {
+               throw new IllegalArgumentException("Invalid aircraft feed URL", e);
+           }
+           om = new ObjectMapper();
+       }
 
     public Flux<Aircraft> getAircraft() {
         List<Aircraft> positions = new ArrayList<>();
--- 8/planefinder/src/main/java/com/thehecklers/planefinder/PlaneFinderService.java
+++ 8/planefinder/src/main/java/com/thehecklers/planefinder/PlaneFinderService.java
@@ -12,6 +12,8 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import reactor.core.publisher.Flux;ackson.databind.ObjectMapper;
+
 import reactor.core.publisher.Flux;
 
 @Service
