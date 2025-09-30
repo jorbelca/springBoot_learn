ad2d000cafc12632bc3cee19d7dcb2fc510e9ed8
j <76847923+jorbelca@users.noreply.github.com>
2025-09-30 18:20:14 +0200

    fin, end

--- 12/aircraft-positions/src/main/java/com/thehecklers/aircraftpositions/Aircraft.java
+++ 12/aircraft-positions/src/main/java/com/thehecklers/aircraftpositions/Aircraft.java
@@ -41,6 +41,49 @@ public class Aircraft {
     private Instant posUpdateTime;
     @JsonProperty("bds40_seen_time")
     private Instant bds40SeenTime;
+ public Aircraft(Long id,
+            String callsign, String squawk, String reg, String flightno,
+            String route, String type, String category,
+            int altitude, int heading, int speed,
+            int vertRate, int selectedAltitude,
+            double lat, double lon, double barometer,
+            double polarDistance, double polarBearing,
+            boolean isADSB, boolean isOnGround,
+            Instant lastSeenTime, Instant posUpdateTime, Instant bds40SeenTime) {
+        this.id = id;
+        this.callsign = callsign;
+        this.squawk = squawk;
+        this.reg = reg;
+        this.flightno = flightno;
+        this.route = route;
+        this.type = type;
+        this.category = category;
+        this.altitude = altitude;
+        this.heading = heading;
+        this.speed = speed;
+        this.vertRate = vertRate;
+        this.selectedAltitude = selectedAltitude;
+        this.lat = lat;
+        this.lon = lon;
+        this.barometer = barometer;
+        this.polarDistance = polarDistance;
+        this.polarBearing = polarBearing;
+        this.isADSB = isADSB;
+        this.isOnGround = isOnGround;
+        this.lastSeenTime = lastSeenTime;
+        this.posUpdateTime = posUpdateTime;
+        this.bds40SeenTime = bds40SeenTime;
+    }
+public Aircraft(String callsign, String reg, String flightno, String type,
+            int altitude, int heading, int speed,
+            double lat, double lon) {
+
+        this(null, callsign, "sqwk", reg, flightno, "route", type, "ct",
+                altitude, heading, speed, 0, 0,
+                lat, lon, 0D, 0D, 0D,
+                false, true,
+                Instant.now(), Instant.now(), Instant.now());
+    }
 
     public String getReg() {
         return this.reg;
